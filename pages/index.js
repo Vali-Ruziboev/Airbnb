b9c163735c411c3a41904b65b0c8daa29dec790f
Vali <valiruziboyev212@gmail.com>
2022-05-26 16:41:33 +0500

    banner

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Banner from '../components/Banner'
 import Header from '../components/Header'
 import styles from '../styles/Home.module.css'
 
@@ -13,6 +14,15 @@ export default function Home() {
       </Head>
 
       <Header />
+      <Banner />
+
+      <main className='max-w-7xl mx-auto px-8 sm:px-16'>
+        <section className='pt-6'>
+          <h2 className='text-4xl font-semibold pb-5'>Explore Nearby</h2>
+
+
+        </section>
+      </main>
     </div>
   )
 }
