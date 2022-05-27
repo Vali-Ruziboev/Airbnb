b771c07c0f612459b2d9c6d8b28c1eb29e3acb29
Vali <valiruziboyev212@gmail.com>
2022-05-27 16:19:38 +0500

    small card

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,10 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Banner from '../components/Banner'
 import Header from '../components/Header'
+import SmallCard from '../components/SmallCard'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function Home({ exploreData }) {
   return (
     <div>
       <Head>
@@ -20,9 +21,28 @@ export default function Home() {
         <section className='pt-6'>
           <h2 className='text-4xl font-semibold pb-5'>Explore Nearby</h2>
 
-
+          <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+            {exploreData?.map(({ img, distance, location })=>(
+              <SmallCard 
+                key={img}
+                img={img}
+                distance={distance}
+                location={location}
+              />
+            ))}
+          </div>
         </section>
       </main>
     </div>
   )
 }
+
+
+export async function getStaticProps(){
+  const exploreData = await fetch('https://links.papareact.com/pyp').then((res)=>res.json())
+  return {
+    props:{
+      exploreData
+    }
+  }
+};
